e0d9e24c805841e921a548171bbbd28a26435ee4
Laura Berge <lauraeberge@gmail.com>
2019-09-14 17:25:23 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,12 +2,21 @@ module Players
   class Computer < Player
     def move(board)
       sleep 1 
+      n = nil
       if board.cells[4] == " "
         n = "5"
       elsif board.cells[2] == " "
         n = "3"
       elsif board.cells[0] == " "
-        n = 
+        n = "1"
+      elsif board.cells[8] == " "
+        n = "9"
+      elsif board.cells[6] == " "
+        n = "7"
+      else
+        until board.valid_move?(n) do
+          n = rand(1, 9)
+        end
       end
       n
     end
