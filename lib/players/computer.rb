9af8446e8b470516490aca506cd03883d292f9a3
Laura Berge <lauraeberge@gmail.com>
2019-09-14 17:42:53 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -38,11 +38,11 @@ module Players
     
     def winning_move?(board)
       WIN_COMBINATIONS.each do |array|
-        if board.cells[array[0]] == self.token && board.cells[array[1]] == self.token
+        if (board.cells[array[0]] == self.token) && (board.cells[array[1]] == self.token)
           return (array[2] + 1).to_s
-        elsif board.cells[array[1]] == self.token && board.cells[array[2]] == self.token
+        elsif (board.cells[array[1]] == self.token) && (board.cells[array[2]] == self.token)
           return (array[2] + 1).to_s
-        elsif board.cells[array[0]] == self.token && board.cells[array[2]] == self.token
+        elsif (board.cells[array[0]] == self.token) && (board.cells[array[2]] == self.token)
           return (array[2] + 1).to_s
         end
         false
@@ -51,11 +51,11 @@ module Players
     
     def block_opponent_win?(board)
       WIN_COMBINATIONS.each do |array|
-        if board.cells[array[0]] == other_player_token && board.cells[array[1]] == other_player_token
+        if (board.cells[array[0]] == other_player_token) && (board.cells[array[1]] == other_player_token)
           return (array[2] + 1).to_s
-        elsif board.cells[array[1]] == other_player_token && board.cells[array[2]] == other_player_token
+        elsif (board.cells[array[1]] == other_player_token) && (board.cells[array[2]] == other_player_token)
           return (array[2] + 1).to_s
-        elsif board.cells[array[0]] == other_player_token && board.cells[array[2]] == other_player_token
+        elsif (board.cells[array[0]] == other_player_token) && (board.cells[array[2]] == other_player_token)
           return (array[2] + 1).to_s
         end
         false
